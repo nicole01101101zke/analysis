af9467588fa8aec91f8beb2975db61a1474ee040
nicole01101101zke <19966422680@163.com>
2024-01-15 15:58:22 +0800

    run rodinia benchmarks on GPU

--- run.py
+++ run.py
@@ -65,7 +65,77 @@ def run_polybench(benchmark_row0, path, run_command):
     else:
         print("No match2 found.")
     
+def run_rodinia(benchmark_row0, path):
+    parboil_directory = "/home/zke/rodinia-main/opencl"
+    os.chdir(parboil_directory)
+    compile_command = f"cd {path} && make clean && make && make --always-make run" # make rundwt2d报错make: 'run' is up to date.
+    result = subprocess.run(compile_command, shell=True, stdout=subprocess.PIPE, text=True)
+    output = result.stdout
+    match = re.search(r'Total:\s*([\d.]+)', output)
+    match_leu = re.search(r'Total application run time:\s*([\d.]+)', output)
 
+    if match_leu:
+        extracted_number = float(match_leu.group(1))
+        print("Extracted Number:", extracted_number)
+    elif match:
+        extracted_number = float(match.group(1))
+        print("Extracted Number:", extracted_number)
+    else:
+        print("No match found.")
+    formatted_number = "{:.10f}".format(extracted_number)
+    
+    workbook = openpyxl.load_workbook('/home/zke/analysis/data.xlsx')
+    sheet = workbook.active
+    for row_number, row in enumerate(sheet.iter_rows(values_only=True), start=1):
+        if row[0] and row[0].startswith(benchmark_row0):
+            sheet.cell(row=row_number, column=12, value=formatted_number)
+            break
+    workbook.save('/home/zke/analysis/data.xlsx')
+
+def run_rodinia_nn(): # 这个看的是Exec:
+    formatted_number = "{:.10f}".format(0.010240)
+    
+    workbook = openpyxl.load_workbook('/home/zke/analysis/data.xlsx')
+    sheet = workbook.active
+    for row_number, row in enumerate(sheet.iter_rows(values_only=True), start=1):
+        if row[0] and row[0].startswith("rodinia_nn"):
+            sheet.cell(row=row_number, column=12, value=formatted_number)
+            break
+    workbook.save('/home/zke/analysis/data.xlsx')
+
+def run_rodinia_pathfinder(): # 很奇怪，编译时找不到timing.h，在这里跑找不到编译好的文件
+    formatted_number = "{:.10f}".format(0.198896) 
+    
+    workbook = openpyxl.load_workbook('/home/zke/analysis/data.xlsx')
+    sheet = workbook.active
+    for row_number, row in enumerate(sheet.iter_rows(values_only=True), start=1):
+        if row[0] and row[0].startswith("rodinia_pathfinder"):
+            sheet.cell(row=row_number, column=12, value=formatted_number)
+            break
+    workbook.save('/home/zke/analysis/data.xlsx')
+
+def run_rodinia_particlefilter(benchmark_row0, command):
+    parboil_directory = "/home/zke/rodinia-main/opencl/particlefilter"
+    os.chdir(parboil_directory)
+    compile_command = f"make clean && make && {command}"
+    result = subprocess.run(compile_command, shell=True, stdout=subprocess.PIPE, text=True)
+    output = result.stdout
+    match = re.search(r'Total:\s*([\d.]+)', output)
+
+    if match:
+        extracted_number = float(match.group(1))
+        print("Extracted Number:", extracted_number)
+    else:
+        print("No match found.")
+    formatted_number = "{:.10f}".format(extracted_number)
+    
+    workbook = openpyxl.load_workbook('/home/zke/analysis/data.xlsx')
+    sheet = workbook.active
+    for row_number, row in enumerate(sheet.iter_rows(values_only=True), start=1):
+        if row[0] and row[0].startswith(benchmark_row0):
+            sheet.cell(row=row_number, column=12, value=formatted_number)
+            break
+    workbook.save('/home/zke/analysis/data.xlsx')
 
 # #run_parboil("parboil_bfs", "bfs", "SF")
 # run_parboil("parboil_cutcp", "cutcp", "large")
@@ -76,25 +146,52 @@ def run_polybench(benchmark_row0, path, run_command):
 # run_parboil("parboil_stencil", "stencil", "default")
 # run_parboil("parboil_tpacf", "tpacf", "large")
 
-run_polybench("polybench_correlation", "datamining/correlation", "./correlation.exe")
-run_polybench("polybench_covariance", "datamining/covariance", "./covariance.exe")
-run_polybench("polybench_2mm", "linear-algebra/kernels/2mm", "./2mm.exe")
-run_polybench("polybench_3mm", "linear-algebra/kernels/3mm", "./3mm.exe")
-run_polybench("polybench_atax", "linear-algebra/kernels/atax", "./atax.exe")
-run_polybench("polybench_bicg", "linear-algebra/kernels/bicg", "./bicg.exe")
-run_polybench("polybench_doitgen", "linear-algebra/kernels/doitgen", "./doitgen.exe")
-run_polybench("polybench_gemm", "linear-algebra/kernels/gemm", "./gemm.exe")
-run_polybench("polybench_gemver", "linear-algebra/kernels/gemver", "./gemver.exe")
-run_polybench("polybench_gesummv", "linear-algebra/kernels/gesummv", "./gesummv.exe")
-run_polybench("polybench_mvt", "linear-algebra/kernels/mvt", "./mvt.exe")
-run_polybench("polybench_syr2k", "linear-algebra/kernels/syr2k", "./syr2k.exe")
-run_polybench("polybench_syrk", "linear-algebra/kernels/syrk", "./syrk.exe")
-run_polybench("polybench_gramschmidt", "linear-algebra/solvers/gramschmidt", "./gramschmidt.exe")
-run_polybench("polybench_lu", "linear-algebra/solvers/lu", "./lu.exe")
-run_polybench("polybench_adi", "stencils/adi", "./adi.exe")
-run_polybench("polybench_convolution-2d", "stencils/convolution-2d", "./2DConvolution.exe")
-run_polybench("polybench_convolution-3d", "stencils/convolution-3d", "./3DConvolution.exe")
-run_polybench("polybench_fdtd-2d", "stencils/fdtd-2d", "./fdtd2d.exe")
-run_polybench("polybench_jacobi-1d-imper", "stencils/jacobi-1d-imper", "./jacobi1D.exe")
-run_polybench("polybench_jacobi-2d-imper", "stencils/jacobi-2d-imper", "./jacobi2D.exe")
+# run_polybench("polybench_correlation", "datamining/correlation", "./correlation.exe")
+# run_polybench("polybench_covariance", "datamining/covariance", "./covariance.exe")
+# run_polybench("polybench_2mm", "linear-algebra/kernels/2mm", "./2mm.exe")
+# run_polybench("polybench_3mm", "linear-algebra/kernels/3mm", "./3mm.exe")
+# run_polybench("polybench_atax", "linear-algebra/kernels/atax", "./atax.exe")
+# run_polybench("polybench_bicg", "linear-algebra/kernels/bicg", "./bicg.exe")
+# run_polybench("polybench_doitgen", "linear-algebra/kernels/doitgen", "./doitgen.exe")
+# run_polybench("polybench_gemm", "linear-algebra/kernels/gemm", "./gemm.exe")
+# run_polybench("polybench_gemver", "linear-algebra/kernels/gemver", "./gemver.exe")
+# run_polybench("polybench_gesummv", "linear-algebra/kernels/gesummv", "./gesummv.exe")
+# run_polybench("polybench_mvt", "linear-algebra/kernels/mvt", "./mvt.exe")
+# run_polybench("polybench_syr2k", "linear-algebra/kernels/syr2k", "./syr2k.exe")
+# run_polybench("polybench_syrk", "linear-algebra/kernels/syrk", "./syrk.exe")
+# run_polybench("polybench_gramschmidt", "linear-algebra/solvers/gramschmidt", "./gramschmidt.exe")
+# run_polybench("polybench_lu", "linear-algebra/solvers/lu", "./lu.exe")
+# run_polybench("polybench_adi", "stencils/adi", "./adi.exe")
+# run_polybench("polybench_convolution-2d", "stencils/convolution-2d", "./2DConvolution.exe")
+# run_polybench("polybench_convolution-3d", "stencils/convolution-3d", "./3DConvolution.exe")
+# run_polybench("polybench_fdtd-2d", "stencils/fdtd-2d", "./fdtd2d.exe")
+# run_polybench("polybench_jacobi-1d-imper", "stencils/jacobi-1d-imper", "./jacobi1D.exe")
+# run_polybench("polybench_jacobi-2d-imper", "stencils/jacobi-2d-imper", "./jacobi2D.exe")
 
+# run_rodinia("rodinia_b+tree", "b+tree")
+# run_rodinia("rodinia_backprop", "backprop")
+# run_rodinia("rodinia_bfs", "bfs")
+# run_rodinia("rodinia_cfd", "cfd") # run操作没写在makefile里，修改了Makefile
+# run_rodinia("rodinia_dwt2d", "dwt2d") # run操作没写在makefile里，修改了Makefile
+# run_rodinia("rodinia_gaussian", "gaussian")
+# run_rodinia("rodinia_heartwall", "heartwall") # linker找不到main.h，把main.h的内容写在了kernel里
+# run_rodinia("rodinia_hotspot", "hotspot")
+# run_rodinia("rodinia_hotspot3D", "hotspot3D")
+# run_rodinia("rodinia_hybridsort", "hybridsort")
+# run_rodinia("rodinia_kmeans", "kmeans") # Segmentation fault (core dumped) 暂时不处理
+# run_rodinia("rodinia_lavaMD", "lavaMD")
+# run_rodinia("rodinia_leukocyte", "leukocyte")
+# run_rodinia("rodinia_lud", "lud")
+# run_rodinia("rodinia_myocyte", "myocyte") #这个的时间消耗很清楚，包括了GPU传输数据的时间
+# run_rodinia_nn() # 这个有交互，需要Enter Platform and Device No (Seperated by Space)
+# run_rodinia("rodinia_nw", "nw")
+# run_rodinia_pathfinder()
+# run_rodinia("rodinia_srad", "srad") # linker找不到main.h，把main.h的内容写在了kernel里
+# run_rodinia("rodinia_streamcluster", "streamcluster")
+
+# ERROR: clGetContextInfo() => CL_INVALID_VALUE
+# ERROR: clGetProgramBuildInfo() => -33
+# run_rodinia_particlefilter("rodinia_particlefilter_naive", "./OCL_particlefilter_naive.out -x 128 -y 128 -z 10 -np 10000")
+
+run_rodinia_particlefilter("rodinia_particlefilter_single","./OCL_particlefilter_single.out -x 128 -y 128 -z 10 -np 400000 $@")
+run_rodinia_particlefilter("rodinia_particlefilter_double","./OCL_particlefilter_double.out -x 128 -y 128 -z 10 -np 400000 $@")
